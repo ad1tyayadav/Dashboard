c9bc5be9f2c0ce6e9c35cf07b5b7c63aaf56a2ed
ad1tyayadav <adityaproworks@gmail.com>
2025-06-03 17:20:20 +0530

    fix placeholder

--- app/lib/placeholder-data.ts
+++ app/lib/placeholder-data.ts
@@ -1,15 +1,15 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const invoices = [
-  {
-    customer_id: customers[0].id,
-    amount: 15795,
-    status: 'pending',
-    date: '2022-12-06',
-  },
-  {
-    customer_id: customers[1].id,
-    amount: 20348,
-    status: 'pending',
-    date: '2022-11-14',
-  },
-];
+// // eslint-disable-next-line @typescript-eslint/no-unused-vars
+// const invoices = [
+//   {
+//     customer_id: customers[0].id,
+//     amount: 15795,
+//     status: 'pending',
+//     date: '2022-12-06',
+//   },
+//   {
+//     customer_id: customers[1].id,
+//     amount: 20348,
+//     status: 'pending',
+//     date: '2022-11-14',
+//   },
+// ];
